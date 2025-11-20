d41db9e796ba1de64aacf801c9cc9c23f3e5f979
codesamu <samufro3@gmail.com>
2025-11-20 10:45:57 +0100

    kisy

---
+++ kisy/code/perceptron/perceptron.py
@@ -0,0 +1,39 @@
+
+
+def perceptron():
+    features=[(0,0), (0,1), (1,0), (1,1)]
+    labels= [0, 0, 0, 1]
+
+    #Gewichte (Bias, w1, w2)
+    w=[0,0,0]
+
+    for epoch in range(10):
+        for x,label in zip(features,labels):
+            # + Bias
+            x=[1]+ list(x)
+
+            #prediction
+            y= sum(wi*xi for wi,xi in zip(w,x))
+            prediction = 1 if y>0 else 0
+
+            # Weights anpassen
+            error= label - prediction
+            if error!=0:
+                for i in range(len(w)):
+                    w[i]+= error*x[i] #alpha = 1
+
+        print(f"new weights in epoch {epoch}: {w}")
+    return w
+
+
+
+
+weights=perceptron()
+
+def predict(x1,x2,w):
+    x=[1,x1,x2]
+    y= sum(wi*xi for wi,xi in zip(w,x))
+    return 1 if y>0 else 0
+
+print(f"Final weights: {weights}")
+print(f"Prediction for (0,0): {predict(0,0,weights)}")
